74522edf9336b84a29e5560408252d6c09608a2f
vsantosh03 <santosh.53v@gmail.com>
2019-09-16 12:12:22 -0400

    Basic test file

---
+++ hello.js
@@ -0,0 +1,28 @@
+/*var lastName = 'post';
+var firstName ='The Washington';
+var fullName = firstName+ " "+ lastName;
+
+console.log(fullName);
+console.log(fullName.length);
+
+
+var math= 29;
+var science= 34;
+var eng= 34;
+var percentage= ((math+ science+ eng)/3);
+console.log('Percentage'+" "+ percentage);
+if (percentage >= 35){
+  console.log("Pass")
+}else {
+console.log("fail");
+}
+*/
+var name = 'vank';
+if (name === 'kumar' || name === 'vsk'){
+  console.log(" your name is San");
+}else if (name === 'vank'){
+  console.log('firstname of san');
+} else
+{
+  console.log('Hi VSK')
+}
